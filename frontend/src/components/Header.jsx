5428045fb1f5a063aa533ea6f9ac1ab324c61837
ranvijay <ranvijay.cool9559@gmail.com>
2026-04-09 11:01:18 +0530

    basic setup

---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div>
+      <div className="text-7xl">Logo</div>
+      <div className="navList">
+        <a href="">Login</a>
+        <a href="">Register</a>
+      </div>
+    </div>
+  )
+}
+
+export default Header
